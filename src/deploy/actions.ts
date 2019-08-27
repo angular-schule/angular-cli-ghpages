286aaddcae8f68256aef85e31d6a4904c9119b04
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-08-27 11:19:35 +0200

    feat: adds --no-build option to skip build, adds alias -c
    This implements the convention from angular-schule/ngx-deploy-starter#1

--- src/deploy/actions.ts
+++ src/deploy/actions.ts
@@ -11,23 +11,28 @@ export default async function deploy(
   options: Schema
 ) {
 
-  if (!context.target) {
-    throw new Error('Cannot execute the build target');
+  if (options.noBuild) {
+    context.logger.info(`📦 Skipping build`);
+  } else {
+
+    if (!context.target) {
+      throw new Error('Cannot execute the build target');
+    }
+
+    const configuration = options.configuration ? options.configuration : 'production'
+    const overrides = {
+      ...(options.baseHref && {baseHref: options.baseHref})
+    };
+
+    context.logger.info(`📦 Building "${ context.target.project }". Configuration: "${ configuration }".${ options.baseHref ? ' Your base-href: "' + options.baseHref + '"' : '' }`);
+
+    const build = await context.scheduleTarget({
+      target: 'build',
+      project: context.target.project,
+      configuration
+    }, overrides as json.JsonObject);
+    await build.result;
   }
-
-  const configuration = options.configuration ? options.configuration : 'production'
-  const overrides = {
-    ...(options.baseHref && {baseHref: options.baseHref})
-  };
-
-  context.logger.info(`📦 Building "${ context.target.project }". Configuration: "${ configuration }".${ options.baseHref ? ' Your base-href: "' + options.baseHref + '"' : '' }`);
-
-  const build = await context.scheduleTarget({
-    target: 'build',
-    project: context.target.project,
-    configuration
-  }, overrides as json.JsonObject);
-  await build.result;
 
   await engine.run(
     projectRoot,

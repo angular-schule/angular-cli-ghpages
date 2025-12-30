e543772eeb332d830289eef82e147777aab83259
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-12-30 14:50:07 +0100

    fix: resolve TypeScript variable redeclaration in tests

--- src/commander-fork/test/test.options.regex.spec.ts
+++ src/commander-fork/test/test.options.regex.spec.ts
@@ -1,13 +1,13 @@
 /**
  * Module dependencies.
  */
-const commander = require('../');
 // var program = require('../')
 //   , should = require('should');
 
 describe('options.regex', () => {
 it('should validate with regex', () => {
-let program = new commander.Command();
+const commanderRegex = require('../');
+let program = new commanderRegex.Command();
 program
   .version('0.0.1')
   .option('-s, --size <size>', 'Pizza Size', /^(large|medium|small)$/i, 'medium')
